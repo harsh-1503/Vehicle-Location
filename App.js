cfdd7670c581961062eec8b94f1f9528d030374d
harsh-1503 <harshkarwa1345@gmail.com>
2023-01-13 21:05:21 +0530

    version 1

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View , Image, TextInput} from 'react-native';
+// import SvgUrl from 
 export default function App() {
+  // const Input=({labe})
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      
+      <View style={styles.rating}>
+        {/* <Image source={require('./my-icon.jpg')} /> */}
+      </View>
+      <TextInput placeholder='Enter first location' onChangeText={(e)=>console.warn(e) }  style={{fontSize:20,borderColor: '#000000',borderWidth: 3,padding: 10,borderRadius: 10,margin: 10,width: '90%'}}></TextInput>
+      <TextInput placeholder='Enter first location' onChangeText={(e)=>console.warn(e) }  style={{fontSize:20,borderColor: '#000000',borderWidth: 3,padding: 10,borderRadius: 10,margin: 10,width: '90%'}}></TextInput>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +23,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-start',
+    marginVertical: 60,
+
+  }
+  
 });
